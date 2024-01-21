662dd44989e6cadbca642affeddbbf8d58c2182c
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-01-22 00:44:36 +0530

    Use native command checking in python instead of command -v

--- install.py
+++ install.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
 import os
+import shutil
 import subprocess
 
+# Define needed functions
 def run_command(command):
   subprocess.run(command, shell=True, check=True)
 
@@ -20,13 +22,15 @@ run_command('scripts/setup-bootloader.py')                     # Set bootloader 
 
 # Setup zsh as user shell
 zsh_env_setup = "printf '### SET XDG DIR FOR ZSH ###\\nZDOTDIR=~/.config/zsh\\n' | sudo tee -a /etc/zsh/zshenv > /dev/null"
-while True:
-  try:
-    run_command(zsh_env_setup)
-    run_command(f'chsh -s "{subprocess.check_output(["command", "-v", "zsh"]).decode().strip()}"')
-    break
-  except subprocess.CalledProcessError:
-    pass
+zsh_path = shutil.which("zsh")
+if zsh_path:
+    try:
+        run_command(zsh_env_setup)
+        run_command(f'chsh -s "{zsh_path}"')
+    except subprocess.CalledProcessError:
+        print("Error setting Zsh as the user shell.")
+else:
+    print("Zsh is not installed or not in the PATH.")
 
 # Set xfce4-terminal as default
 run_command('sudo update-alternatives --set x-terminal-emulator /usr/bin/xfce4-terminal.wrapper')
